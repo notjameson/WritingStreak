4937782282e98c0ccc15c73a862031c8b9d3b639
Jameson Zaballos <jamesonzaballos@gmail.com>
2024-01-05 22:11:14 -0600

    Working!

--- background.js
+++ background.js
@@ -1,29 +1,17 @@
-// background.js
-
-// Variable to store word count
 let wordCount = 0;
-
-// Listener for messages from content scripts
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log('start background listener')
-    if (request.type === "updateWordCount") {
-        // Update word count
-        console.log('update worker count listener detected')
-        wordCount = request.count;
-    }
-    sendResponse("response sent")
-});
 
 // Function to send the current word count to the popup
 function sendWordCountToPopup() {
     chrome.runtime.sendMessage({ type: "wordCount", count: wordCount });
 }
 
-// Listener for popup requests
+// Listener for messages from content scripts
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log("message received")
+    if (request.type === "updateWordCount") {
+        wordCount = request.count;
+    }
     if (request.type === "getWordCount") {
         sendWordCountToPopup();
-        sendResponse("response sent")
     }
+    sendResponse("Event logged: " + request.type)
 });
--- popup.js
+++ popup.js
@@ -1,7 +1,8 @@
+chrome.runtime.sendMessage({ type: "getWordCount" })
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if (request.wordCount) {
-      document.getElementById('wordCount').textContent = request.wordCount;
+    if (request.type === "wordCount") {
+      document.getElementById('count').textContent = request.count;
     }
   }
 );
