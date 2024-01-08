ddda76ee23ec3fe0ab641660c71e725cf38bcac4
Jameson Zaballos <jamesonzaballos@gmail.com>
2024-01-07 20:13:38 -0600

    Adding gitignore and on/off icons

--- background.js
+++ background.js
@@ -1,4 +1,8 @@
 let wordCount = 0;
+let onIconPath = ({ "16": "/Icons/streak-16.png",
+"32": "/Icons/streak-32.png" })
+let offIconPath = ({ "16": "/Icons/streak-off-16.png",
+"32": "/Icons/streak-off-32.png" })
 
 // Function to send the current word count to the popup
 function sendWordCountToPopup() {
@@ -12,6 +16,13 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     }
     if (request.type === "getWordCount") {
         sendWordCountToPopup();
+    }
+    if (request.type === "iconState") {
+        console.log(request.value)
+        let iconPath = request.value ? onIconPath : offIconPath;
+        chrome.action.setIcon({
+            path: iconPath
+        })
     }
     sendResponse("Event logged: " + request.type)
 });
--- popup.js
+++ popup.js
@@ -1,4 +1,8 @@
+let state = true;
+
 chrome.runtime.sendMessage({ type: "getWordCount" })
+chrome.runtime.sendMessage({ type: "iconState", value: state})
+state = !state
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if (request.type === "wordCount") {
