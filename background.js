140fc7bfcb2b69f431057946c64dbe0f2294fd4f
Jameson Zaballos <jamesonzaballos@gmail.com>
2024-01-09 22:22:36 -0600

    Clarifying

--- background.js
+++ background.js
@@ -5,15 +5,16 @@ let onIconPath = ({ "16": "/Icons/streak-16.png",
 let offIconPath = ({ "16": "/Icons/streak-off-16.png",
 "32": "/Icons/streak-off-32.png" })
 
-// Function to send the current word count to the popup
 function sendWordCountToPopup() {
     chrome.runtime.sendMessage({ type: "wordCount", count: wordCount });
 }
 
 // Listener for messages from content scripts
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+
     if (request.type === "updateWordCount") {
         if (state) {
+            // only update if extension is active
             wordCount += 1;
         }
     }
