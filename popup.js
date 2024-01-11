8bf19651277c0a95ff1fa24c6aad570fe6af696b
Jameson Zaballos <jamesonzaballos@gmail.com>
2024-01-10 23:55:42 -0600

    extra comment

--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
 chrome.runtime.sendMessage({ type: "getWordCount" })
+// send every time popup is opened
 chrome.runtime.sendMessage({ type: "updateState" })
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
